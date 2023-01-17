9fe6e82a1a348af1b700bf3f1b2155576439cbae
Arounou <122846267+Arounou@users.noreply.github.com>
2023-01-17 19:47:57 -0300

    Add files via upload

---
+++ Arounou/SPOOFER.bat
@@ -0,0 +1,9 @@
+AMIDEWINx64.exe /su
+AMIDEWINx64.exe /bs SHADOWZIN-%random%-%random%-Spoofer
+AMIDEWINx64.exe /ss SHADOWZIN-%random%-%random%-Spoofer
+AMIDEWINx64.exe /id SHADOWZIN-%random%-Spoofer
+AMIDEWINx64.exe /iv SHADOWZIN-%random%-Spoofer
+AMIDEWINx64.exe /ivn SHADOWZIN-%random%-Spoofer
+AMIDEWINx64.exe /sm SHADOWZIN PREMIUN
+AMIDEWINx64.exe /sp SHADOWZIN
+exit
